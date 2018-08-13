c66a77093b24d0a97b4f0bd51cd9705b600d7607
yizeliang <yizeliang@126.com>
2018-08-13 10:27:07 +0800

    Update AndroidLogAdapter.kt

--- logger/src/main/java/com/orhanobut/logger/kt/AndroidLogAdapter.kt
+++ logger/src/main/java/com/orhanobut/logger/kt/AndroidLogAdapter.kt
@@ -4,26 +4,32 @@ import android.util.Log
 
 internal class AndroidLogAdapter : LogAdapter {
     override fun d(tag: String, message: String) {
+        Thread.sleep(1)
         Log.d(tag, message)
     }
 
     override fun e(tag: String, message: String) {
+        Thread.sleep(1)
         Log.e(tag, message)
     }
 
     override fun w(tag: String, message: String) {
+        Thread.sleep(1)
         Log.w(tag, message)
     }
 
     override fun i(tag: String, message: String) {
+        Thread.sleep(1)
         Log.i(tag, message)
     }
 
     override fun v(tag: String, message: String) {
+        Thread.sleep(1)
         Log.v(tag, message)
     }
 
     override fun wtf(tag: String, message: String) {
+        Thread.sleep(1)
         Log.wtf(tag, message)
     }
 }
