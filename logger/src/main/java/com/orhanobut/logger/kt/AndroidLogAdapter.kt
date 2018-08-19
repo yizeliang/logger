d4d349d64db859ed9014a85c69f122d05470745e
yize <yizeliang@126.com>
2018-08-19 16:32:04 +0800

    3.2.3-release

--- logger/src/main/java/com/orhanobut/logger/kt/AndroidLogAdapter.kt
+++ logger/src/main/java/com/orhanobut/logger/kt/AndroidLogAdapter.kt
@@ -5,41 +5,45 @@ import android.os.Looper
 import android.util.Log
 
 internal class AndroidLogAdapter : LogAdapter {
+    companion object {
+        const val display = 10L
+    }
+
     private val handle = Handler(Looper.getMainLooper())
     override fun d(tag: String, message: String) {
         handle.postDelayed({
             Log.d(tag, message)
-        }, 1)
+        }, display)
     }
 
     override fun e(tag: String, message: String) {
         handle.postDelayed({
             Log.e(tag, message)
-        }, 1)
+        }, display)
 
     }
 
     override fun w(tag: String, message: String) {
         handle.postDelayed({
             Log.w(tag, message)
-        }, 1)
+        }, display)
     }
 
     override fun i(tag: String, message: String) {
         handle.postDelayed({
             Log.i(tag, message)
-        }, 1)
+        }, display)
     }
 
     override fun v(tag: String, message: String) {
         handle.postDelayed({
             Log.v(tag, message)
-        }, 1)
+        }, display)
     }
 
     override fun wtf(tag: String, message: String) {
         handle.postDelayed({
             Log.wtf(tag, message)
-        }, 1)
+        }, display)
     }
 }
